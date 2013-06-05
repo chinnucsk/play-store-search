b9f4db88d45940c797458b13184468202f088058
Javier C <youracow@gmail.com>
2013-06-05 00:35:07 -0600

    first commit

---
+++ src/google/play/store/search/GooglePlayStoreSearch.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google.play.store.search;
+
+/**
+ *
+ * @author javierAle
+ */
+public class GooglePlayStoreSearch {
+    
+}
---
+++ src/google/play/store/search/Main.java
@@ -0,0 +1,23 @@
+
+package google.play.store.search;
+
+/**
+ *
+ * @author Javier C
+ *
+ */
+public class Main {
+
+    public Main() {
+        new GooglePlayStoreSearch();
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Main();
+    }
+}
